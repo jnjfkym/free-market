e5a995043572e0d6acf0fbc7ec6f4ad5799ca9c2
junji-fukuyama <junji.fukuyama@di-v.co.jp>
2020-08-04 15:32:18 +0900

    third commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,22 @@
+# config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.12.1'
+lock '3.14.1'
 
 # Capistranoのログの表示に利用する
 set :application, 'free-market'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:usagrammer/free-market.git'
+set :repo_url,  'git@github.com:junji-fukuyama/free-market.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.6.5です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/free-market_linux2.pem']
+                  keys: ['~/.ssh/junjifukuyama.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.150.52', user: 'ec2-user', roles: %w{app db web}
+server '175.41.230.28', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================

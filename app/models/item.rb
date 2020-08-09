f27577b16fedb8ab6783330515d5cd86a92bf6e3
junji-fukuyama <junji.fukuyama@di-v.co.jp>
2020-08-10 01:09:34 +0900

    put_up_item_image_no_js

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true, update_only: true
   belongs_to :category
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :images, length: { minimum: 1, maximum: 5, message: "の数が不正です" }
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 

9ab1ef49bc9a63986962bd17a9668effb6151f01
junji-fukuyama <junji.fukuyama@di-v.co.jp>
2020-08-15 23:25:10 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,8 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'free-market19890508'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/free-market0508'
+    config.fog_directory  = 'free-maket19890508'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/free-maket19890508'
   else
     ## 開発環境の時
     config.storage = :file
